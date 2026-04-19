5522de3d05ef748663ebf041219d43eeca781541
Poornima c <poornima2005c@gmail.com>
2026-04-20 01:50:39 +0530

    added Product.java model class

---
+++ cartify/build/classes/model/Product.java
@@ -0,0 +1,114 @@
+package model;
+
+public class Product {
+	private int productId;
+	private String productName;
+	private double price;
+	private int quantity;
+	private String image;
+	private String brand;
+	private String description;
+	//private  Date created_at
+	public Product() {
+		super();
+	}
+	public Product(String productName, double price, int quantity, String image, String brand, String description) {
+		super();
+		this.productName = productName;
+		this.price = price;
+		this.quantity = quantity;
+		this.image = image;
+		this.brand = brand;
+		this.description = description;
+	}
+	/**
+	 * @return the productId
+	 */
+	public int getProductId() {
+		return productId;
+	}
+	/**
+	 * @param productId the productId to set
+	 */
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	/**
+	 * @return the productName
+	 */
+	public String getProductName() {
+		return productName;
+	}
+	/**
+	 * @param productName the productName to set
+	 */
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	/**
+	 * @return the quantity
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	/**
+	 * @return the image
+	 */
+	public String getImage() {
+		return image;
+	}
+	/**
+	 * @param image the image to set
+	 */
+	public void setImage(String image) {
+		this.image = image;
+	}
+	/**
+	 * @return the brand
+	 */
+	public String getBrand() {
+		return brand;
+	}
+	/**
+	 * @param brand the brand to set
+	 */
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", price=" + price + ", quantity="
+				+ quantity + ", image=" + image + ", brand=" + brand + ", description=" + description + "]";
+	}
+
+}
